4a57b9aef1c076d0dde98f68f2210d9d180301b2
Alina Kozyreva <alina0206200119@gmail.com>
2020-09-30 22:43:28 +0300

    feat: functions implemented

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
-exports.min = function min (array) {
+exports.min = function min(array) {
+  if (Array.isArray(array) && array.length !== 0){
+    return Math.min(...array);
+  }
   return 0;
 }
 
-exports.max = function max (array) {
+exports.max = function max(array) {
+  if (Array.isArray(array) && array.length !== 0){
+    return Math.max(...array);
+  }
   return 0;
 }
 
-exports.avg = function avg (array) {
+exports.avg = function avg(array) {
+  if (Array.isArray(array) && array.length !== 0){
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    return array.reduce(reducer)/array.length;
+  }
   return 0;
 }
